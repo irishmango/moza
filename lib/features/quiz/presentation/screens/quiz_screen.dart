29e06e00d47d60e4ef57e611ef4465c64276ca5e
Shokri Francis Raoof <raoofs.de@gmail.com>
2025-05-14 12:06:37 +0200

    tidied up

--- lib/features/quiz/presentation/screens/quiz_screen.dart
+++ lib/features/quiz/presentation/screens/quiz_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:moza/features/quiz/domain/dummy_quiz.dart';
-import 'package:moza/features/quiz/presentation/widgets/progress_bar.dart';
+import 'package:moza/features/quiz/presentation/screens/quiz_results_screen.dart';
+import 'package:moza/features/quiz/presentation/widgets/heading_w_progress_bar.dart';
+import 'package:moza/features/quiz/presentation/widgets/quiz_choice_button.dart';
+import 'package:moza/features/quiz/presentation/widgets/quiz_main_button.dart';
 import 'package:moza/shared/custom_scaffold_quiz.dart';
 
 class QuizScreen extends StatefulWidget {
@@ -30,9 +33,7 @@ class _QuizScreenState extends State<QuizScreen> {
             SizedBox(height: 16,),
 
             Container(
-              decoration: BoxDecoration(
-                border: Border.all()
-              ),
+              
               child: Column(
                 children: [
                       SizedBox(height: 60,),
@@ -41,6 +42,69 @@ class _QuizScreenState extends State<QuizScreen> {
                       if (currentQuestion.imagePath != null) 
                       SizedBox(height: 114, width: 200, child: Image.asset(currentQuestion.imagePath!), 
                       ),
+
+                      SizedBox(height: 40,),
+                      ...List.generate(currentQuestion.options.length, (index) {
+                      final isSelected = selectedIndex == index;
+                      final isCorrect = currentQuestion.correctIndex == index;
+                      final answered = isAnswered;
+
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 6.0),
+                        child: QuizChoiceButton(
+                          text: currentQuestion.options[index],
+                          isSelected: isSelected,
+                          isCorrect: isCorrect,
+                          isAnswered: isAnswered,
+                          onPressed: () {
+                            if (!answered) {
+                              setState(() {
+                                selectedIndex = index;
+                              });
+                            }
+                          }
+                        ),
+                      );
+                    }),
+
+                    SizedBox(height: 45),
+
+                    QuizMainButton(
+                      onPressed: selectedIndex == null && !hasChecked
+                          ? null
+                          : () {
+                              setState(() {
+                                if (!hasChecked) {
+                                  hasChecked = true;
+                                  isAnswered = true;
+                                } else {
+                                  if (selectedIndex == currentQuestion.correctIndex) {
+                                    result++;
+                                  }
+
+                                  if (currentQuestionIndex < quiz.length - 1) {
+                                    currentQuestionIndex++;
+                                    selectedIndex = null;
+                                    hasChecked = false;
+                                    isAnswered = false;
+                                  } else {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (_) => QuizResultsScreen(
+                                          result: result,
+                                          total: quiz.length,
+                                        ),
+                                      ),
+                                    );
+                                  }
+                                }
+                              });
+                            },
+                      hasChecked: hasChecked,
+                    )
+
+
                     ],
               ),
             ),
